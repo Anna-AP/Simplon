2c0e795ae91fa2381e4b611cc546bb95010e0735
Silanie de Trévière <Silanie de Trvire>
2016-11-19 13:55:48 +0100

    mise à jour exercices

--- 2-Aout/Semaine2/3-CvGraphique/mysql/creationdatabase.sql
+++
@@ -1,4 +0,0 @@
-create database if not exists siteCV character set utf8 collate utf8_unicode_ci;
-use siteCV;
-
-grant all privileges on siteCV.* to 'siteCV_user'@'localhost' identified by 'C@r@bistouilleD@nsL@Choucroute10011990';
